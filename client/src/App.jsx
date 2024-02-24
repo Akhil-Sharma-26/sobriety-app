b87d02fd90ba9b2a6c9ed272b5c07d7f12f91f79
Ritishm05 <rmahajan_be22@thapar.edu>
2024-02-25 00:36:38 +0530

    profile page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Profilepage from './pages/Profilepage'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +11,7 @@ function App() {
     <>
       {/* <Header /> */}
       <Outlet />
+      <Profilepage/>
       {/* <Footer /> */}
     </>
   )
