f4cd7e223fec845ae216b3a2b470c675fe91452b
Ank206 <akapoor1_be22@thapar.edu>
2024-02-25 04:41:31 +0530

    .

--- client/src/pages/Profilepage.jsx
+++ client/src/pages/Profilepage.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+// import React,{useState,useEffect} from 'react'
 // const Profilepage = () => {
 //         const [streak, setStreak] = useState(0);
 //         const [maxStreak, setMaxStreak] = useState(0);
