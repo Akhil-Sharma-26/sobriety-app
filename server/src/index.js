e070d1ac7be2d78ec5a32b5680ed3d9f00260b77
Akhil-Sharma-26 <2004akhil26sharma@gmail.com>
2024-02-24 15:59:12 +0530

    Init Setup of server and client DONE

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+// import connectDB from "./db/DB_connect.js";
+// import mongoose from "mongoose";
+// import DB_NAME from "./constants.js";
+// import  express  from "express";
+// import cors from "cors";
+import { app } from "./app.js";
+// const app = express();
+
+dotenv.config({
+    path: "./.env"
+}); // to use the .env file
+
+
+app.listen(process.env.PORT || 8000,()=>{ // app.listen is used to listen to the port
+    console.log(`Server is running on PORT: ${process.env.PORT}`);
+})
